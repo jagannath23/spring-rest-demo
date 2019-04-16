b9f9f88f88ad045a26102d1e4d849b4b1f3598a2
Juan I. Castellano <37382853+jagganath23@users.noreply.github.com>
2019-04-16 00:15:03 -0300

    source code added, with pom and gitignore

---
+++ src/main/java/jagannath/demo/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package jagannath.demo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import jagannath.demo.entity.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
